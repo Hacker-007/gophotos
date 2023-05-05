79ce4acf43456f6bb35ff92f0f2b4436468eefab
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2023-05-05 00:41:41 -0400

    added page transitions and start of waitlist page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+
 import { Inter } from "next/font/google";
+
+import type { AppProps } from "next/app";
 import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div
       className={`${inter.className} flex h-screen w-screen flex-col bg-zinc-100 px-16 pt-7`}
     >
-      <nav className="flex h-10 items-center justify-between">
+      <nav className="h-10 w-full flex items-center justify-between">
         <Link href="/" className="font-semibold">
           Artisan<span className="text-xl">.</span>
         </Link>
@@ -20,7 +23,9 @@ export default function App({ Component, pageProps }: AppProps) {
           </Link>
         </div>
       </nav>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
     </div>
   );
 }
---
+++ pages/waitlist.tsx
@@ -0,0 +1,9 @@
+import AnimatedPage from "@/components/AnimatedPage";
+
+export default function WaitlistPage() {
+  return (
+    <AnimatedPage className="h-full w-full">
+      <div>Waitlist Page!</div>
+    </AnimatedPage>
+  );
+}
